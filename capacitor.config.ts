d2dd5908623ebba3f13f5e6fbd3738456575f017
koluod25-cyber <koluod25@gmail.com>
2026-09-25 06:55:46 +0800

    Anton-Service-Pos

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.antonservice.pos',
+  appName: 'Anton Service POS',
+  webDir: 'dist/public',
+};
+
+export default config;
