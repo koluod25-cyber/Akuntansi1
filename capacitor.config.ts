6ccab182c2468ce592325f10b64b9fbf337bb743
koluod25-cyber <koluod25@gmail.com>
2026-09-25 08:52:13 +0800

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.antonservice.pos',
-  appName: 'Anton Service POS',
-  webDir: 'dist/public',
-};
-
-export default config;
